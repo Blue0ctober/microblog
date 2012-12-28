2946e734f5f66f337568f40455785558e22034a7
Edgar Valdes <edgargvaldes@gmail.com>
2012-12-27 22:26:16 -0800

    helps if i add a comma after line 20 (users = users,)

--- app/__init__.py
+++ app/__init__.py
@@ -2,3 +2,4 @@ from flask import Flask
 
 app = Flask(__name__)
 from app import views
+
--- app/views.py
+++ app/views.py
@@ -17,5 +17,5 @@ def index():
     ]
     return render_template("index.html",
         title = 'Home',
-        user = user
+        user = user,
         posts = posts)
