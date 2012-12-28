e2ae197116d17a526479b76ce14be24e97fd12e3
Edgar Valdes <edgargvaldes@gmail.com>
2012-12-27 22:21:03 -0800

    added to view.py fake posts

--- app/views.py
+++ app/views.py
@@ -5,6 +5,17 @@ from app import app
 @app.route('/index')
 def index():
     user = { 'nickname': 'Edgar' } # fake user
+    posts = [ #fake array of posts
+        {
+            'author': { 'nickname': 'John' },
+            'body': 'Beautiful day in LA!'
+        },
+        {
+            'author': { 'nickname': 'Susan' },
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
     return render_template("index.html",
         title = 'Home',
-        user = user)
+        user = user
+        posts = posts)
