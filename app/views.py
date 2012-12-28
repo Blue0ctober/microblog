c69e208c888470a5bb209cd093daa3c4805dc8e3
Edgar Valdes <edgargvaldes@gmail.com>
2012-12-28 00:26:37 -0800

    added form validation on submit  to views loginform()

--- app/views.py
+++ app/views.py
@@ -7,6 +7,9 @@ from forms import LoginForm
 @app.route('/login', methods = ['GET','POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for OpenID="' + form.openid.data + '", remember_me=' + str(form.remember_me.data))
+        return redirect('/index')
     return render_template('login.html',
         title = 'Sign In',
         form = form)
