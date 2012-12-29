9159f84fae4165710386c52a68335fd2f315740a
Edgar Valdes <edgargvaldes@gmail.com>
2012-12-28 20:53:33 -0800

    added sqlachemy to config and init

--- config.py
+++ config.py
@@ -1,3 +1,10 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+SQLACHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLACHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+
+
 CSRF_ENABLED = True
 SECRET_KEY = 'blah blah blah secret'
 
