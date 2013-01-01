edafdd439497a9bd870aa7a755c70e6423a0e0a9
Edgar Valdes <edgargvaldes@gmail.com>
2012-12-31 18:10:08 -0800

    added after login to view

--- app/views.py
+++ app/views.py
@@ -41,3 +41,23 @@ def index():
 @lm.user_loader
 def load_user(id):
     return User.query.get(int(id))
+
+@oid.afer_login
+def after_login(resp):
+    if resp.email is None or resp.email == "":
+        flash('Invalid login. Please try again.')
+        redirect(url_for('login'))
+    user = User.query.filter_by(email = resp.email).first(()
+    if user is None:
+        nickname = resp.nickname
+        if nickname is None or nickname == "":
+            nickname = resp.email.split('@')[0]
+        user = User(nickname = nickname, email = resp.email, role = ROLE_USER)
+        db.session.add(user)
+        db.session.commit()
+    remember_me = False
+    if 'remember_me' in session:
+        remember_me = session['remember_me']
+        session.pop('remember_me', None)
+    login_user(user, remember = remember_me)
+    return redirect(request.arge.get('next') or url_for('index'))
