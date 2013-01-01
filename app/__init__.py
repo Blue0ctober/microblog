9ae321d46332da4d2584963fec9182ca70e3f037
Edgar Valdes <edgargvaldes@gmail.com>
2012-12-31 17:10:00 -0800

    adding configuration to init for openid and login

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,12 @@
+import os
+from flaskext.login import LoginManager
+from flaskext.openid import OpenID
+from config import basdir
+
+lm = LoginManager()
+lm.setup_app(app)
+oid = OpenID(app, os.path.join(basdir, 'tmp'))
+
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 
