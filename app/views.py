3cd27aca4091fc74fc63ef79fdc9b5c7fb73ddb0
Edgar Valdes <edgargvaldes@gmail.com>
2012-12-31 17:43:04 -0800

    added some modules to views.py and redid config to pull from db

--- app/views.py
+++ app/views.py
@@ -1,15 +1,19 @@
-from flask import render_template, flash, redirect
-from app import app
+from flask import render_template, flash, redirect, session, url_for, request, g
+from flaskext.login import login_user, logout_user, current_user, login_required
+from app import app, db, lm, oid
 from forms import LoginForm
+from models import User, ROLE_USER, ROLE_ADMIN
 
 # index view function supressed for brevity
 
 @app.route('/login', methods = ['GET','POST'])
 def login():
+    if g.user is not None and g.user.is_authenticated():
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for OpenID="' + form.openid.data + '", remember_me=' + str(form.remember_me.data))
-        return redirect('/index')
+        session['remember_me'] = form.remember_me.data
+        return oid.try_login(from.openid.data, ask_for = ['nickname', 'email'])
     return render_template('login.html',
         title = 'Sign In',
         form = form,
@@ -33,3 +37,7 @@ def index():
         title = 'Home',
         user = user,
         posts = posts)
+
+@lm.user_loader
+def load_user(id):
+    return User.query.get(int(id))
