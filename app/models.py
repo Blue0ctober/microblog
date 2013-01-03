3b46eb6943e2cf426da9083cdf6ff9f78d6111fe
Edgar Valdes <edgargvaldes@gmail.com>
2013-01-03 15:41:39 -0800

    added 2 columns to db for about me and last seen under users

--- app/models.py
+++ app/models.py
@@ -10,7 +10,9 @@ class User(db.Model):
     email = db.Column(db.String(120), index = True, unique = True)
     role = db.Column(db.SmallInteger, default = ROLE_USER)
     posts = db.relationship('Post', backref = 'author', lazy = 'dynamic')
-
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime)
+    
     def is_authenticated(self):
         return True
 
@@ -25,7 +27,7 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User %r>' % (self.name)
-        
+
     def avatar(self, size):
         return 'http://www.gravatar.com/avatar/' + md5(self.email).hexdigest() + '?d=mm&s' + str(size)
 
