a08ab1dc4c06e7fa84a4806ee3fa4940de2dff02
Edgar Valdes <edgargvaldes@gmail.com>
2013-01-02 19:12:49 -0800

    fixed a few bugs and issues

--- app/__init__.py
+++ app/__init__.py
@@ -1,22 +1,16 @@
-
+import os
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.login import LoginManager
+from flask.ext.openid import OpenID
+from config import basedir
 
 app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
+lm = LoginManager()
+lm.init_app(app)
+lm.login_view = 'login'
+oid = OpenID(app, os.path.join(basedir, 'tmp'))
 
 from app import views, models
-
-lm = LoginManager()
-lm.setup_app(app)
-lm.login_view = 'login'
-
-import os
-from flaskext.login import LoginManager
-from flaskext.openid import OpenID
-from config import basedir
-
-lm = LoginManager()
-lm.setup_app(app)
-oid = OpenID(app, os.path.join(basedir, 'tmp'))
--- app/views.py
+++ app/views.py
@@ -1,12 +1,39 @@
 from flask import render_template, flash, redirect, session, url_for, request, g
-from flaskext.login import login_user, logout_user, current_user, login_required
+from flask.ext.login import login_user, logout_user, current_user, login_required
 from app import app, db, lm, oid
 from forms import LoginForm
 from models import User, ROLE_USER, ROLE_ADMIN
 
-# index view function supressed for brevity
+@lm.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 
-@app.route('/login', methods = ['GET','POST'])
+@app.before_request
+def before_request():
+    g.user = current_user
+    
+@app.route('/')
+@app.route('/index')
+@login_required
+def index():
+    user = g.user
+    posts = [
+        { 
+            'author': { 'nickname': 'John' }, 
+            'body': 'Beautiful day in Portland!' 
+        },
+        { 
+            'author': { 'nickname': 'Susan' }, 
+            'body': 'The Avengers movie was so cool!' 
+        }
+    ]
+    return render_template('index.html',
+        title = 'Home',
+        user = user,
+        posts = posts)
+
+@app.route('/login', methods = ['GET', 'POST'])
+@oid.loginhandler
 def login():
     if g.user is not None and g.user.is_authenticated():
         return redirect(url_for('index'))
@@ -14,36 +41,12 @@ def login():
     if form.validate_on_submit():
         session['remember_me'] = form.remember_me.data
         return oid.try_login(form.openid.data, ask_for = ['nickname', 'email'])
-    return render_template('login.html',
+    return render_template('login.html', 
         title = 'Sign In',
         form = form,
         providers = app.config['OPENID_PROVIDERS'])
 
-@app.route('/')
-@app.route('/index')
-@login_required
-def index():
-    user = g.user
-    posts = [
-        {
-            'author': { 'nickname': 'John' },
-            'body': 'Beautiful day in LA!'
-        },
-        {
-            'author': { 'nickname': 'Susan' },
-            'body': 'The Avengers movie was so cool!'
-        }
-    ]
-    return render_template("index.html",
-        title = 'Home',
-        user = user,
-        posts = posts)
-
-@lm.user_loader
-def load_user(id):
-    return User.query.get(int(id))
-
-@oid.afer_login
+@oid.after_login
 def after_login(resp):
     if resp.email is None or resp.email == "":
         flash('Invalid login. Please try again.')
@@ -61,8 +64,9 @@ def after_login(resp):
         remember_me = session['remember_me']
         session.pop('remember_me', None)
     login_user(user, remember = remember_me)
-    return redirect(request.arge.get('next') or url_for('index'))
+    return redirect(request.args.get('next') or url_for('index'))
 
-@app.before_request
-def before_request():
-        g.user = current_user
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
