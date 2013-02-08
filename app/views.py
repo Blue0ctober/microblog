180f3ffa831ff4aa6cee052ab9da9614065e0c7a
Edgar Valdes <edgargvaldes@gmail.com>
2013-02-08 14:13:18 -0800

    added error handlers for 404 and 500

--- app/views.py
+++ app/views.py
@@ -108,3 +108,12 @@ def edit():
         form.about_me.data = g.user.about_me
     return render_template('edit.html',
         form = form)
+
+@app.errorhandler(404)
+def internal_error(error):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_error(error):
+    db.session.rollback()
+    return render_template('500.html'), 500
