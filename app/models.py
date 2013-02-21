62d046aa537b8229e69a09616c112fef016e08e8
Edgar Valdes <edgargvaldes@gmail.com>
2013-02-21 11:38:05 -0800

    added followed and followers to db

--- app/models.py
+++ app/models.py
@@ -4,6 +4,11 @@ from hashlib import md5
 ROLE_USER = 0
 ROLE_ADMIN = 1
 
+followers = db.Table('followers',
+    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+)
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     nickname = db.Column(db.String(64), index = True, unique = True)
@@ -12,6 +17,12 @@ class User(db.Model):
     posts = db.relationship('Post', backref = 'author', lazy = 'dynamic')
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime)
+    followed = db.relationship('User',
+        secondary = followers,
+        primaryjoin = (followers.c.follower_id == id),
+        secondaryjoin = (followers.c.followed_id == id),
+        backref = db.backref('followers', lazy = 'dynamic'),
+        lazy = 'dynamic')
     
     def is_authenticated(self):
         return True
