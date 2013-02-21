ebfc0036c0301154a41155d810173c25d7230a63
Edgar Valdes <edgargvaldes@gmail.com>
2013-02-21 13:52:55 -0800

    added to test.py to test followers

--- tests.py
+++ tests.py
@@ -33,5 +33,32 @@ class TestCase(unittest.TestCase):
         nickname2 = User.make_unique_nickname('john')
         assert nickname2 != 'john'
         assert nickname2 != nickname
+
+    def test_follow(self):
+        u1 = User(nickname = 'john', email = 'john@example.com')
+        u2 = User(nickname = 'susan', email = 'susan@example.com')
+        db.session.add(u1)
+        db.session.add(u2)
+        db.session.commit()
+        assert u1.unfollow(u2) == None
+        u = u1.follow(u2)
+        db.session.add(u)
+        db.session.commit()
+        assert u1.follow(u2) == None
+        assert u1.is_following(u2)
+        assert u1.followed.count() == 1
+        assert u1.followed.first().nickname == 'susan'
+        assert u2.followers.count() == 1
+        assert u2.followers.first().nickname == 'john'
+        u = u1.unfollow(u2)
+        assert u != None
+        db.session.add(u)
+        db.session.commit()
+        assert u1.is_following(u2) == False
+        assert u1.followed.count() == 0
+        assert u2.followers.count() == 0
+
 if __name__ == '__main__':
     unittest.main()
+
+        
